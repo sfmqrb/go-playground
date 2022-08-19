12e67ab302adf49395325d5733398a3bae94de17
maghrebi <sajjad.f.maghrebi@digikala.com>
2022-08-19 17:16:55 +0430

    array and more types

---
+++ flowcontrol.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print("Go runs on ")
+	os := runtime.GOOS
+	fmt.Println(os)
+
+	switch os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+}
