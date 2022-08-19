cdbc8f418c70183db5b899e07eb01baf1332d337
maghrebi <sajjad.f.maghrebi@digikala.com>
2022-08-19 15:13:59 +0430

    using while

--- sqrt.go
+++ sqrt.go
@@ -19,8 +19,20 @@ func newtonsMethod(x float64, mainNumber float64, numOfRec int64) (float64, int6
 	var newX = (1.0 / 2) * (x + mainNumber/x)
 	return newtonsMethod(newX, mainNumber, numOfRec)
 }
+func sqrtNewtonsMethodUsingWhile(mainNumber float64) (float64, int64) {
+	x := 1.0
+	var numOfRec int64 = 1
+	for math.Abs(math.Pow(x, 2)-mainNumber) > precision {
+		x = (1.0 / 2) * (x + mainNumber/x)
+		numOfRec += 1
+	}
+	return x, numOfRec
+}
 
 func main() {
 	x, numOfRec := sqrtNewtonsMethod(81 * 9)
+	fmt.Println(x, numOfRec)
+
+	x, numOfRec = sqrtNewtonsMethodUsingWhile(81 * 9)
 	fmt.Println(x, numOfRec)
 }
