56256d3914a2db18e9ee390e427405f17111b023
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-03-20 10:22:46 +0330

    my conway

--- golang-initials/conway-game-of-life.go
+++ golang-initials/conway-game-of-life.go
@@ -8,86 +8,73 @@ import (
 	"time"
 )
 
-// Field represents a two-dimensional field of cells.
-type Field struct {
-	s    [][]bool
-	w, h int
+type Ground struct {
+	space [][]bool
+	w, h  int
 }
 
-// NewField returns an empty field of the specified width and height.
-func NewField(w, h int) *Field {
-	s := make([][]bool, h)
-	for i := range s {
-		s[i] = make([]bool, w)
+func NewGround(w, h int) *Ground {
+	space := make([][]bool, h)
+	for i := range space {
+		space[i] = make([]bool, w)
 	}
-	return &Field{s: s, w: w, h: h}
+	return &Ground{space: space, w: w, h: h}
 }
 
-// Set sets the state of the specified cell to the given value.
-func (f *Field) Set(x, y int, b bool) {
-	f.s[y][x] = b
+func (g *Ground) Set(x, y int, b bool) {
+	g.space[y][x] = b
 }
 
-// Alive reports whether the specified cell is alive.
-// If the x or y coordinates are outside the field boundaries they are wrapped
-// toroidally. For instance, an x value of -1 is treated as width-1.
-func (f *Field) Alive(x, y int) bool {
-	x += f.w
-	x %= f.w
-	y += f.h
-	y %= f.h
-	return f.s[y][x]
+func (g *Ground) GetValidXY(x, y int) (valid_x, valid_y int) {
+	x += g.w
+	x %= g.w
+	y += g.h
+	y %= g.h
+	return x, y
 }
 
-// Next returns the state of the specified cell at the next time step.
-func (f *Field) Next(x, y int) bool {
-	// Count the adjacent cells that are alive.
+func (g *Ground) Alive(x, y int) bool {
+	return g.space[y][x]
+}
+
+func (g *Ground) Next(x, y int) bool {
 	alive := 0
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
-			if (j != 0 || i != 0) && f.Alive(x+i, y+j) {
+			valid_x, valid_y := g.GetValidXY(x+i, y+j)
+			if (j != 0 || i != 0) && g.Alive(valid_x, valid_y) {
 				alive++
 			}
 		}
 	}
-	// Return next state according to the game rules:
-	//   exactly 3 neighbors: on,
-	//   exactly 2 neighbors: maintain current state,
-	//   otherwise: off.
-	return alive == 3 || alive == 2 && f.Alive(x, y)
+	return alive == 3 || alive == 2 && g.Alive(x, y)
 }
 
-// Life stores the state of a round of Conway's Game of Life.
 type Life struct {
-	a, b *Field
+	a, b *Ground
 	w, h int
 }
 
-// NewLife returns a new Life game state with a random initial state.
 func NewLife(w, h int) *Life {
-	a := NewField(w, h)
+	a := NewGround(w, h)
 	for i := 0; i < (w * h / 4); i++ {
 		a.Set(rand.Intn(w), rand.Intn(h), true)
 	}
 	return &Life{
-		a: a, b: NewField(w, h),
+		a: a, b: NewGround(w, h),
 		w: w, h: h,
 	}
 }
 
-// Step advances the game by one instant, recomputing and updating all cells.
 func (l *Life) Step() {
-	// Update the state of the next field (b) from the current field (a).
 	for y := 0; y < l.h; y++ {
 		for x := 0; x < l.w; x++ {
 			l.b.Set(x, y, l.a.Next(x, y))
 		}
 	}
-	// Swap fields a and b.
 	l.a, l.b = l.b, l.a
 }
 
-// String returns the game board as a string.
 func (l *Life) String() string {
 	var buf bytes.Buffer
 	for y := 0; y < l.h; y++ {
@@ -105,9 +92,9 @@ func (l *Life) String() string {
 
 func main() {
 	l := NewLife(40, 15)
-	for i := 0; i < 300; i++ {
+	for {
 		l.Step()
-		fmt.Print("\x0c", l) // Clear screen and print field.
+		fmt.Print("\x0c", l)
 		time.Sleep(time.Second / 30)
 	}
 }
