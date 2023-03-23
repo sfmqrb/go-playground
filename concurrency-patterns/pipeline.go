4775677325c8c0ddce1f6199b65e348353586d7e
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-03-23 12:39:12 +0330

    adding pipeline

---
+++ concurrency-patterns/pipeline.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	gen := func(nums ...int) <-chan int {
+		out := make(chan int)
+		go func() {
+			for _, n := range nums {
+				out <- n
+			}
+			close(out)
+		}()
+		return out
+	}
+
+	sq := func(in <-chan int) <-chan int {
+		out := make(chan int)
+		go func() {
+			for n := range in {
+				out <- n * n
+			}
+			close(out)
+		}()
+		return out
+	}
+
+	// Set up the pipeline and consume the output.
+	for n := range sq(sq(gen(2, 3))) {
+		fmt.Println(n) // 16 then 81
+	}
+}
